4e2b52f3fe8314d27e641d35497de516fb345099
chamini <ireshikachamini94@gmail.com>
2018-12-18 09:56:40 -0500

    loginPage

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/main', 'MainController@index');
+// Route::post('/main/checkLogin','MainController@checkLogin');
+// Route::get('/main/successLogin','MainController@successLogin');
+// Route::get('/main/logout','MainController@logout');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
